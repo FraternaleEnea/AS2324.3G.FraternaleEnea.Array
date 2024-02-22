0b7dc576fe8d0fb66094de8a6bacae54c32ca5c1
PC05 <PC05@INFO5-5376>
2024-02-22 09:37:20 +0100

    aggiornamento del main e creazione della seconda funzione

--- AS2324.3G.FraternaleEnea.Array/Program.cs
+++ AS2324.3G.FraternaleEnea.Array/Program.cs
@@ -6,8 +6,13 @@
         {
             Console.WriteLine("inserisci quante persone siete");
             int dim=int.Parse(Console.ReadLine());
-            double[] peso= new double[dim];
+            double[] pesi= new double[dim];
             int[] eta=new int[dim];
+            double media = 0;
+            double min = 0;
+            double max = 0;
+            CaricaVettori(ref pesi, ref eta);
+            Statistiche(ref pesi, ref media, ref min, ref max);
 
         }
         static void CaricaVettori(ref double[] pesi, ref int[] eta)
@@ -18,6 +23,25 @@
                 pesi[i]=random.Next(50,101);
                 eta[i] = random.Next(18, 100);
             }
+        }
+        static void Statistiche(ref double[] pesi, ref double media, ref double min, ref double max)
+        {
+            min = pesi[0];
+            max = pesi[0];
+            double somma = 0;
+            for(int i = 0; i < pesi.Length; i++)
+            {
+                if (pesi[i] > max)
+                {
+                    max = pesi[i];
+                }
+                if (pesi[i] < min)
+                {
+                    min = pesi[i];
+                }
+                somma += pesi[i];
+            }
+            media = somma / pesi.Length;
         }
     }
 }
