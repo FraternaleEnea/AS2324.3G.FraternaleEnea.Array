8a87f60482ddd580a528061443a33bad0c26d20a
PC05 <PC05@INFO5-5376>
2024-02-22 09:20:36 +0100

    creazione del main e realizzazione della prima funzione

--- AS2324.3G.FraternaleEnea.Array/Program.cs
+++ AS2324.3G.FraternaleEnea.Array/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("inserisci quante persone siete");
+            int dim=int.Parse(Console.ReadLine());
+            double[] peso= new double[dim];
+            int[] eta=new int[dim];
+
+        }
+        static void CaricaVettori(ref double[] pesi, ref int[] eta)
+        {
+            Random random = new Random();
+            for(int i = 0; i < eta.Length; i++)
+            {
+                pesi[i]=random.Next(50,101);
+                eta[i] = random.Next(18, 100);
+            }
         }
     }
 }
